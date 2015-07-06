be2bf2d301a52af407335d8f9dc3eda3b2919c7e
songjianhua2012 <songjianhua2012@outlook.com>
2015-07-06 20:46:48 +0800

    section-1修改了全局变量

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,15 +1,14 @@
-var same_words = [];
-
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var same_words = [];
   for(var x=0; x<collection_a.length; ++x)
   {
-    findCollectionb(collection_a[x],object_b);
+    findCollectionb(collection_a[x],object_b,same_words);
   }
   return same_words;
 }
 
-function findCollectionb(item,collection_b)
+function findCollectionb(item,collection_b,same_words)
 {
   for(var y=0; y<collection_b.value.length; ++y)
   {
